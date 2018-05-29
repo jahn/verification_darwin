972f98ca2385e5d7fda8adc3d348b2a603d5d6e1
Oliver Jahn <jahn@mit.edu>
2018-05-29 17:49:47 -0400

    replace RT_nlam by nlam

--- darwin_eccov3_6+2/code/RADTRANS_SIZE.h
+++ darwin_eccov3_6+2/code/RADTRANS_SIZE.h
@@ -8,8 +8,8 @@ C #include RADTRANS_SIZE.h
 C    !DESCRIPTION:
 C Contains dimensions and index ranges for radtrans model.
 
-      integer RT_nlam
-      parameter(RT_nlam=13)
+      integer nlam
+      parameter(nlam=13)
 
 CEOP
 #endif /* ALLOW_RADTRANS */
