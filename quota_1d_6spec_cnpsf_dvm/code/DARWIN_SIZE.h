2b01a76dd04b1ea31ae947ebddb3756d61e04898
Oliver Jahn <jahn@mit.edu>
2025-06-14 15:00:38 -0400

    Add 1d dvm exp

---
+++ quota_1d_6spec_cnpsf_dvm/code/DARWIN_SIZE.h
@@ -0,0 +1,24 @@
+#ifdef ALLOW_DARWIN
+
+CBOP
+C    !ROUTINE: DARWIN_SIZE.h
+C    !INTERFACE:
+C #include DARWIN_SIZE.h
+
+C    !DESCRIPTION:
+C Contains dimensions and index ranges for cell model.
+
+      integer nplank, nGroup, nopt
+      integer nPhoto
+      parameter(nopt=1)
+      parameter(nplank=10)
+      parameter(nGroup=9)
+      parameter(nPhoto=6)
+
+#ifndef ALLOW_RADTRANS
+      integer nlam
+      parameter(nlam=1)
+#endif
+
+CEOP
+#endif /* ALLOW_DARWIN */
